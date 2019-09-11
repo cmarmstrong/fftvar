53bd9231b2617b2877c9cf2266a58f874260e33b
cmarmstrong <omni.armstrong@gmail.com>
2019-09-10 23:24:21 -0500

    fix syntax errors in roxygen

--- R/variogram.R
+++ R/variogram.R
@@ -1,23 +1,26 @@
-## ' X <- matrix(c(3,6,5,7,2,2,4,NaN,0), nrow=3, byrow=TRUE) ## input
-## ' fftvario(X)
+#' X <- matrix(c(3,6,5,7,2,2,4,NaN,0), nrow=3, byrow=TRUE) ## input
+#' fftvario(X)
+#' @export
 fftvario <- function(X) {
+    browser()
     D <- 2*dim(X)-1 - dim(X)
-    X <- do.call(rbind, c(list(X), rep(NaN, D[1])))
-    X <- do.call(cbind, c(list(X), rep(NaN, D[2])))
-    Xid <- !is.nan(X)
+    X <- do.call(rbind, c(list(X), rep(NA, D[1])))
+    X <- do.call(cbind, c(list(X), rep(NA, D[2])))
+    Xid <- !is.na(X)
     X[!Xid] <- 0
 
     fx <- fft(X)
     fx2 <- fft(X*X)
     fid <- fft(Xid)
 
-    nh11 <- round(Re(fft(Conj(fid) * fid, inverse=TRUE)))
+    nh11 <- round(Re(fft(Conj(fid) * fid, inverse=TRUE))) # n_pairwise*nrow*ncol
     gh11 <- Re(fft(Conj(fid) * fx2 + Conj(fx2) * fid - 2 * Conj(fx) * fx, inverse=TRUE))
-    gh11 <- gh11 / nh11 / 2
-    nh11 <- nh11 / nrow(nh11) * ncol(nh11) # normalize count matrix
+    gh11 <- gh11 / nh11 / 2 # nrow * ncol cancels out (see next line)
+    nh11 <- nh11 / nrow(nh11) * ncol(nh11) # normalize
     return(list(fftshift(nh11), fftshift(gh11)))
 }
 
+#' @describeIn variogram
 fftshift <- function(X) {
     f <- function(X) {
         lefthalf <- ceiling(ncol(X)/2)
